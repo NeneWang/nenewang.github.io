8937cadb65bd2b88abe3cde1ed996c72aa07c395
NelsonWang <wangnelson2@gmail.com>
2024-03-11 03:19:57 -0400

    added requiremennts and demo

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 export PYDEVD_DISABLE_FILE_VALIDATION=1
 
 # jupyter-book clean .
-
+pip install -r requirements.txt
 jupyter-book build .
 
 ghp-import -n -p -f ./_build/html --cname='neuraldatascience.io'
